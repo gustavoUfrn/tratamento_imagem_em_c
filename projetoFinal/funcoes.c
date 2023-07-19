0b916756bbf1f17f7043c59519720dcccaf36bda
Ewerton <116316377+carvalhoewerton@users.noreply.github.com>
2023-07-19 18:00:10 -0300

    Update funcoes.c

--- projetoFinal/funcoes.c
+++ projetoFinal/funcoes.c
@@ -142,8 +142,7 @@ void blur(ptn_header Header){
 
 void sharpening(ptn_header Header) {
     int media_red, media_green, media_blue;
-
-    header Header_aux;
+    ptn_header Header_aux;
 
     Header_aux.altura = Header->altura;
     Header_aux.largura = Header->largura;
@@ -153,22 +152,22 @@ void sharpening(ptn_header Header) {
     for (int i = 1; i < Header->altura - 1; i++) {
         for (int j = 1; j < Header->largura - 1; j++) {
             // Lógica para red
-            media_red = (Header->pixel_img[i-1][j-1].red + Header->pixel_img[i-1][j].red + Header->pixel_img[i-1][j+1].red +
-                         Header->pixel_img[i][j-1].red + Header->pixel_img[i][j].red * 8 + Header->pixel_img[i][j+1].red +
-                         Header->pixel_img[i+1][j-1].red + Header->pixel_img[i+1][j].red + Header->pixel_img[i+1][j+1].red) - (9 * Header->pixel_img[i][j].red);
+            media_red = (9 * Header->pixel_img[i][j].red) - (Header->pixel_img[i-1][j-1].red + Header->pixel_img[i-1][j].red + Header->pixel_img[i-1][j+1].red +
+                           Header->pixel_img[i][j-1].red + Header->pixel_img[i][j+1].red +
+                           Header->pixel_img[i+1][j-1].red + Header->pixel_img[i+1][j].red + Header->pixel_img[i+1][j+1].red);
             // Lógica para green
-            media_green = (Header->pixel_img[i-1][j-1].green + Header->pixel_img[i-1][j].green + Header->pixel_img[i-1][j+1].green +
-                           Header->pixel_img[i][j-1].green + Header->pixel_img[i][j].green * 8 + Header->pixel_img[i][j+1].green +
-                           Header->pixel_img[i+1][j-1].green + Header->pixel_img[i+1][j].green + Header->pixel_img[i+1][j+1].green) - (9 * Header->pixel_img[i][j].green);
+            media_green = (9 * Header->pixel_img[i][j].green) - (Header->pixel_img[i-1][j-1].green + Header->pixel_img[i-1][j].green + Header->pixel_img[i-1][j+1].green +
+                             Header->pixel_img[i][j-1].green + Header->pixel_img[i][j+1].green +
+                             Header->pixel_img[i+1][j-1].green + Header->pixel_img[i+1][j].green + Header->pixel_img[i+1][j+1].green);
             // Lógica para blue
-            media_blue = (Header->pixel_img[i-1][j-1].blue + Header->pixel_img[i-1][j].blue + Header->pixel_img[i-1][j+1].blue +
-                          Header->pixel_img[i][j-1].blue + Header->pixel_img[i][j].blue * 8 + Header->pixel_img[i][j+1].blue +
-                          Header->pixel_img[i+1][j-1].blue + Header->pixel_img[i+1][j].blue + Header->pixel_img[i+1][j+1].blue) - (9 * Header->pixel_img[i][j].blue);
+            media_blue = (9 * Header->pixel_img[i][j].blue) - (Header->pixel_img[i-1][j-1].blue + Header->pixel_img[i-1][j].blue + Header->pixel_img[i-1][j+1].blue +
+                            Header->pixel_img[i][j-1].blue + Header->pixel_img[i][j+1].blue +
+                            Header->pixel_img[i+1][j-1].blue + Header->pixel_img[i+1][j].blue + Header->pixel_img[i+1][j+1].blue);
 
             // Aplicar o valor calculado no pixel correspondente da matriz auxiliar
-            Header_aux.pixel_img[i][j].red = Header->pixel_img[i][j].red + media_red;
-            Header_aux.pixel_img[i][j].green = Header->pixel_img[i][j].green + media_green;
-            Header_aux.pixel_img[i][j].blue = Header->pixel_img[i][j].blue + media_blue;
+            Header_aux.pixel_img[i][j].red = clamp(Header->pixel_img[i][j].red + media_red, 0, 255);
+            Header_aux.pixel_img[i][j].green = clamp(Header->pixel_img[i][j].green + media_green, 0, 255);
+            Header_aux.pixel_img[i][j].blue = clamp(Header->pixel_img[i][j].blue + media_blue, 0, 255);
         }
     }
 
