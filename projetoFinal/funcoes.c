5d986a9f7145c6c7678b964a037ee35e86d813d8
Gustavo Alessandro <mxgugaa@gmail.com>
2023-07-20 17:01:45 -0300

    comentarios

--- projetoFinal/funcoes.c
+++ projetoFinal/funcoes.c
@@ -1,7 +1,11 @@
 #include "funcoes.h"
 
-// Todas as fuinções foram explicadas no readme.md na parte "Definições de funções do projeto (funcoes.h)"
+// A maioria das funções usa como parâmetro o ponteiro de Header para que seja possivel fazer alterações na matriz ou nas variaveis de altura e largura.
 
+// Após usar Header_aux podemos liberar o espaço alocado em Header, e tornar os valores de Header_aux como o novo de Header(desalocado na função imprime_arquivo).
+
+// matriz_pixels ira fazer alocação dinamica da matriz pixel_img que esta em header, com base na altura, largura e o tamanho de Pixel.
+// Ira receber como parâmetro Header, para que seja póssivel acessar pixel_img, altura e largura.
 void matriz_pixels(ptn_header Header){
     int i;
 
@@ -11,6 +15,8 @@ void matriz_pixels(ptn_header Header){
 	}
 }
 
+// ler_arquivo vai usar a entrada de dados padrão para salvar os dados em header e no valor alocado pixel_img.
+// Para isso é necessario chamar matriz_pixels passando header como parêmetro.
 void ler_arquivo(ptn_header Header){
     int i, j;
     
@@ -30,6 +36,8 @@ void ler_arquivo(ptn_header Header){
     return;
 }
 
+// ler_arguentos vai precisa como parâmetro o inteiro que é passado como argumento e ponteiro que contém os argumentos.
+// Essa função vai retorna o ponteiro char com o argumento que foi repassada para ao executar o arquivo.
 char* ler_argumentos(int pnt_int, char **pnt_char){
     int i=1;
 
@@ -40,6 +48,9 @@ char* ler_argumentos(int pnt_int, char **pnt_char){
     }
 };
 
+// Usa como parâmetro header que será passado para a função de alteração de imagem que será usada em seguida, e também o ponteiro com
+// o parâmetro da função ler_argumentos
+// Para que a próxima função seja chamada comparamos a variavel Operacao com a string da função usando strcmp que deverá ser usada em seguida.
 void operacao(char* Operacao, ptn_header Header){
 
     if( strcmp(Operacao, "gray") == 0){
@@ -57,6 +68,8 @@ void operacao(char* Operacao, ptn_header Header){
     }
 };
 
+// Quando chamada a função vai usar os valores dos pixels red/green/blue em uma posição no array para criar uma média, após isso os pixels red/green/blue
+// precisamos redefinir os valores dos pixels red/green/blue usados para essa média
 void gray(ptn_header Header) {
     int i, j;
 
@@ -73,6 +86,11 @@ void gray(ptn_header Header) {
     return;
 }
 
+// Rotate usa a variavel Header_aux vai armazenar os novos valores de altura e largura, além dos novos valores da matriz
+// note que é preciso alocar um espaço apra Header_aux
+// Para que seja possivel rotacionar basta espelhar a matriz e depois inverte-lá (ponta-cabeça)
+// Header_aux.pixel_img[j][i]; -> Espelha a imagem
+// [Header->altura - 1 - i] -> Ao enves de contar de 0 até altura, conta de altura até 0 -> Inverte a matriz
 void rotate(ptn_header Header){
     int i, j;
 
@@ -98,6 +116,10 @@ void rotate(ptn_header Header){
     Header->pixel_img = Header_aux.pixel_img;
 }
 
+// Para que a função blur funcionar precisamos ter em mente que os pixels que seram utilizados como base não podem ser o pixels da borda da matriz]
+// A parte do código responsavel por evitar isso está no if -> (i!=0 && j!=0) && (i!=(Header->altura)-1 && j!=(Header->largura)-1)
+// Ao percorer a Matriz, usamos os pixels que estão a volta do pixel(uma matriz 3x3) escolhido para criar uma média que será aplicada
+// em uma matriz 3x3 a partir do pixel inicial (neste caso, não é o pixel central da matriz e sim o pixel em uma linha e coluna antes do pixel central).
 void blur(ptn_header Header){
     int media_red, media_green, media_blue;
     header Header_aux;    
@@ -141,6 +163,14 @@ void blur(ptn_header Header){
     Header->pixel_img = Header_aux.pixel_img;
 }
 
+// Na função de enlarge precisamos que Header_aux armazene os novos valores de altura e largura, que é calculado com o dobro -1 -> ((altura + altura) -1)
+// Nesta função é necessario que todos os valores de pixel_img estejam zerados a primeira cadeia de fors é resposavel por zerar a matriz
+// A segunda cadeia de fors faz com que os valores da matriz inicial seja expandida, basta multiplicar por dois, assim a matriz incial irá andar
+// de um em um, a a matriz aumentada de 2 em 2.
+// Após o passo anteiror, a matriz alocada em Header_aux ficará espaçada com zeros(Já que anda de 2 em 2).
+// O primeiro if fará com que as linhas sejam preenchidas fazendo uma média dos pixels adjacentes.
+// O segundo if será responsavel por preencher as colunas, com a média dos valores acima e abaixo.
+// O terceiro if ira preencher os zeros criados com os valores dos pixels adjacentes, acima e abaixo.
 void enlarge(ptn_header Header){
     int altura_ampliada = Header->altura + (Header->altura-1);
     int largura_ampliada = Header->largura + (Header->largura-1);
@@ -205,6 +235,9 @@ void enlarge(ptn_header Header){
     Header->pixel_img = Header_aux.pixel_img;
 }
 
+// O reduce usa uma parte da lógica do enlarge, porém usamos os pixels da matriz maior, e usamos como média para criar uma matriz menor,
+// Neste caso estamos percorrendo a nova matriz de um em um, porém percorremos a matriz maior de 2 em 2
+// Os novos valores da matriz são a media do pixel escolhido, o pixel abaixo, o pixel adjacente da direita e o pixel abaixo adjacente.
 void reduce(ptn_header Header){
 
     header Header_aux;
@@ -228,6 +261,7 @@ void reduce(ptn_header Header){
     Header->pixel_img = Header_aux.pixel_img;
 }
 
+// Imprime o arquivo com entrada básica de dados e libera o espaço que ainda está sendo utilizado na alocação dinâmica
 void imprime_arquivo(ptn_header Header){
     int i, j;
 
